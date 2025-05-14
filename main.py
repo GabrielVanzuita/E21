69893e805720527acbb4e65ba82f0f61f09427a0
Gabriel_Vanzuita <141075948+GabrielVanzuita@users.noreply.github.com>
2025-05-14 19:29:06 -0300

    Lógica básica de menu adicionada

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ load_dotenv()  # Carrega as variáveis do .env
 
 API_KEY = os.getenv("API_KEY")
 MAX_ARTICLES = 20
+url:
 
 if not API_KEY:
     raise ValueError(" NEWSAPI_KEY não encontrada. Verifique seu arquivo .env.")
@@ -28,3 +29,22 @@ def get_noticias (url,API_KEY):
         return []
 
     return data["articles"]
+
+while True:
+    print ('\n PESQUISA DE NOTÍCIAS')
+    topic = input ("Digite um tema para buscar ou 'Sair', para sair.").lower()
+    max_articles = 10
+    if topic == "sair":
+        break
+    else:
+        try:
+            limit = int (input(f"Quantas notícias você deseja visualizar?Máximo de {max_articles}:")
+        except ValueError:
+            if limit > 10 or limit < 1:
+                print ("Número de notícias exibidas é inválido. Por favor, escolha um número entre 1 e 10")
+            else:
+                print("Apenas números são permitidos")
+                continue
+
+        articles = get_noticias(url=url, API_KEY=API_KEY)
+
