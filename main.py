0b055dec7e985142539850a3b20a8ea778a40962
Gabriel_Vanzuita <141075948+GabrielVanzuita@users.noreply.github.com>
2025-05-14 19:20:34 -0300

    Criada função básica de formatação e requisição de notícias

--- main.py
+++ main.py
@@ -10,3 +10,21 @@ MAX_ARTICLES = 20
 if not API_KEY:
     raise ValueError(" NEWSAPI_KEY não encontrada. Verifique seu arquivo .env.")
 
+def get_noticias (url,API_KEY):
+    params = {
+        "q": topic,
+        "pageSize": limit,
+        "sortBy": "publishedAt",
+        "apiKey": API_KEY,
+        "language": "eng"
+    }
+
+    response = requests.get(url, params = params)
+
+    data = response.json()
+
+    if response.status_code != 200 or "articles" not in data:
+        print("rro ao buscar notícias:", data.get("message", "Erro desconhecido"))
+        return []
+
+    return data["articles"]
